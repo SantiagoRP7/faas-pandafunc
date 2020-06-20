3d2a2e328d3352649c35390ac65863de4083090d
SantiagoRP7 <rodriguez.santiago@correounivalle.edu.co>
2020-06-19 23:30:16 -0500

    Update handler.py

--- pandafunc/handler.py
+++ pandafunc/handler.py
@@ -1,7 +1,21 @@
+
+import requests
+import pandas as pd
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
+    
+    download_url = "https://www.datos.gov.co/resource/jhpq-24h2.csv"
+target_csv_path = "jhpq-24h2.csv"
 
+response = requests.get(download_url)
+response.raise_for_status()    # Check that the request was successful
+with open(target_csv_path, "wb") as f:
+    f.write(response.content)
+#print("Download ready.")
+nba = pd.read_csv("jhpq-24h2.csv")
+type(nba)
     return req
